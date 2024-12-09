0296205d0d55cb311511567c603ac730150d8e4b
Luca Novello <gnovello@myseneca.ca>
2024-12-09 02:50:06 -0500

    updated assignment comments

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
-WEB322 – Assignment 05
+WEB322 – Assignment 06
 I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
 No part of this assignment has been copied manually or electronically from any other source 
 (including 3rd party web sites) or distributed to other students. I acknowledge that violation
@@ -8,7 +8,7 @@ the course.
 
 Name:                   Luca Novello
 Student ID:             038515003
-Date:                   12-4-2024
+Date:                   12-11-2024
 Vercel Web App URL:     https://web322app-lucanovello.vercel.app/
 GitHub Repository URL:  https://github.com/lucanovello/web322-app
 ********************************************************************************/
